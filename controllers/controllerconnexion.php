87b66c714f4387967bab50f02edf779bd6591aa3
Maxime <moostafarai@hotmail.fr>
2021-12-19 18:41:28 +0100

    modif page connexion && => ||

--- controllers/controllerconnexion.php
+++ controllers/controllerconnexion.php
@@ -7,7 +7,7 @@ $password = '$2a$12$XgX1smkbHgiyFkGhgHUKLOFcwTgzO6esk1tLKQSj9AVErl8/Edh8O';
 if (!empty($_POST)) {
     if (empty($_POST["login"]) || empty($_POST["password"])) {
         $arrayError["error"] = "Veuillez saisir votre mot de passe et/ou login";
-    } elseif (!password_verify($_POST["login"], $login) && !password_verify($_POST["password"], $password)) {
+    } elseif (!password_verify($_POST["login"], $login) || !password_verify($_POST["password"], $password)) {
         $arrayError["error"] = "Mot de passe ou login invalide";
     } else {
         header("Location: welcome.php");
